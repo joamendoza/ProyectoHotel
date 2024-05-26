8a66218d4aaee4ffd3ca8484fac4afe5bd7d54dd
Mxttosierrv <mat.abarca@duocuc.cl>
2024-05-25 20:57:21 -0400

    Initial commit

---
+++ scriptGeneral.js
@@ -0,0 +1,7 @@
+/* 
+    ## Este es el scrip general para manejar cosas como mantener los botones activos,
+    ## funciones pequeñas, cambiar imagenes a conveniencia, mostrar modales, entre otros.
+
+    ## Para hacer cosas más detalladas como, por ejemplo, gestionar un carrito de compras y
+    ## todo eso, se utilizarán .js especificos para ello. Este namás es para tonteras chicas.
+*/

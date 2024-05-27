5eb52ab796b6ca9fe13f6358dd8b0e77795d94b7
Mxttosierrv <mat.abarca@duocuc.cl>
2024-05-27 17:58:10 -0400

    Implementación de modales 'registro usuario' e 'inicio de sesión'

--- scriptGeneral.js
+++ scriptGeneral.js
@@ -5,3 +5,21 @@
     ## Para hacer cosas más detalladas como, por ejemplo, gestionar un carrito de compras y
     ## todo eso, se utilizarán .js especificos para ello. Este namás es para tonteras chicas.
 */
+
+document.addEventListener('DOMContentLoaded', function () {
+    const registrateLink = document.getElementById('registrate');
+    const modalRegistro = new bootstrap.Modal(document.getElementById('modalRegistro'));
+    const iniciarSesionLink = document.getElementById('iniciarSesion');
+    const modalIniciarSesion = new bootstrap.Modal(document.getElementById('modalIniciarSesion'));
+
+    // Muestra el modal de registro al hacer clic en "Regístrate"
+    registrateLink.addEventListener('click', function (event) {
+        event.preventDefault();
+        modalRegistro.show();
+    });
+    // Muestra el modal de inicio de sesión al hacer clic en "Iniciar sesión"
+    iniciarSesionLink.addEventListener('click', function (event) {
+        event.preventDefault();
+        modalIniciarSesion.show();
+    });
+});
